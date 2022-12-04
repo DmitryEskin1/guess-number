7d822f513ef6d5bacd3778ce29c620f69b727350
DmitryEskin1 <99074259+DmitryEskin1@users.noreply.github.com>
2022-12-04 19:12:33 +0300

    3

--- src/View.php
+++ src/View.php
@@ -1,8 +1,30 @@
 <?php
 
-namespace DmitryEskin1\guess_number\View;
+    namespace DmitryEskin1\guess_number\View;
 
-function showGame()
+    use function DmitryEskin1\guess_number\Controller\showGame;
+    use function DmitryEskin1\guess_number\Controller\saySalute;
+    use function DmitryEskin1\guess_number\Controller\setting;
+
+function startGame()
 {
-    echo "Game shown".PHP_EOL;
+    echo "Game shown" . PHP_EOL;
+    setting();
+    saySalute();
+    showGame();
+}
+
+function showList()
+{
+    echo "List all saved games from DB \n";
+}
+
+function showReplays()
+{
+    echo "Replay all moves from the game wit ID \n";
+}
+
+function showTopRating()
+{
+    echo "Show stats for players form DB \n";
 }
